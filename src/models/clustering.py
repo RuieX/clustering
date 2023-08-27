d2363b0729d333e664791f50124fc771f16f3837
RuieX <runjiexia28@gmail.com>
2023-08-28 00:24:11 +0200

    refactoring for easier generality

--- src/models/clustering.py
+++ src/models/clustering.py
@@ -26,13 +26,16 @@ ModelType = TypeVar("ModelType", MeanShift, SpectralClustering, GaussianMixture)
 class ClusteringModelEvaluation(ABC):
     """
     ClusteringModelEvaluation, an abstract class to implement multiple evaluation
-    over multiple hyper-parameters and dimensionality
+    over multiple hyperparameters and dimensionality
 
     This class automatize different clustering models evaluation over a different combination of:
         - hyperparameter (size of kernel, number of clusters)
         - number of components
     Provide some methods for analyzing evaluation results, such as getting the best model or plotting some trends
     """
+    model_type: Optional[ModelType] = None
+    model_name: str = 'model'
+    hyperparameter_name: str = 'hyperparameter'
 
     def __init__(self, data: Dataset, n_components: List[int], hyperparam_vals: List[int | float]):
         """
@@ -49,7 +52,7 @@ class ClusteringModelEvaluation(ABC):
         self._results_bestmodels: Dict[int, Dict[str, ModelType | float]] = dict()
         self._best_model: Dict[str, ModelType | float] = dict()
 
-    def _evaluate(self, model: ModelType, model_name, hyperparam_name: str):
+    def evaluate(self):
         """
         Evaluate a ClusteringModel over all combination of
             - number of components used
@@ -58,7 +61,7 @@ class ClusteringModelEvaluation(ABC):
             - number of clusters found
             - random index score of any model
             - evaluation time
-        :param model: implementation of a specific clustering model
+        :param model: implementation of a specific clustering model TODO delete this line
         """
         components_results = {}  # n_components : dictionary keyed by hyperparameter
         components_bestmodels = {}
@@ -71,8 +74,8 @@ class ClusteringModelEvaluation(ABC):
             hyperparameters = {}
 
             for k in tqdm(self._hyperparam_values, desc='', leave=False):
-                tqdm.write(f'Processing {hyperparam_name} value: {k}')
-                model = model.set_params(**{hyperparam_name: k})
+                tqdm.write(f'Processing {self.hyperparameter_name} value: {k}')
+                model = self.model_type.set_params(**{self.hyperparameter_name: k})
                 t1 = time.perf_counter()
                 labels = model.fit_predict(data_pca.x)
                 t2 = time.perf_counter()
@@ -90,7 +93,7 @@ class ClusteringModelEvaluation(ABC):
                     best_score_lcl = score
                     components_bestmodels[n] = {
                         'model': copy.deepcopy(model),
-                        f'{hyperparam_name}': k,
+                        f'{self.hyperparameter_name}': k,
                         'score': score,
                         'n_clusters': len(set(labels)),
                         'time': elapsed
@@ -102,7 +105,7 @@ class ClusteringModelEvaluation(ABC):
                     self._best_model = {
                         'model': copy.deepcopy(model),
                         'n_components': n,
-                        f'{hyperparam_name}': k,
+                        f'{self.hyperparameter_name}': k,
                         'score': score,
                         'n_clusters': len(set(labels)),
                         'time': elapsed
@@ -115,7 +118,7 @@ class ClusteringModelEvaluation(ABC):
         self._evaluated = True
 
         # Save the results to a JSON file using the constructed filename
-        self._save_result(model_name=model_name, hyperparam_name=hyperparam_name)
+        self._save_result()
 
     def _is_evaluated(self):
         """
@@ -126,27 +129,27 @@ class ClusteringModelEvaluation(ABC):
             raise Exception("Model has not been evaluated yet.")
         return self._evaluated
 
-    def _save_result(self, model_name: str, hyperparam_name: str):
+    def _save_result(self):
         """
         :return:
         """
         if not os.path.exists(get_results_dir()):
             os.mkdir(get_results_dir())
 
-        result_name = os.path.join(get_results_dir(), f"{model_name}_{hyperparam_name}_result.json")
-        result_bestmod_name = os.path.join(get_results_dir(), f"{model_name}_{hyperparam_name}_result_bestmodels.pkl")
-        bestmod_name = os.path.join(get_results_dir(), f"{model_name}_{hyperparam_name}_bestmodel.pkl")
+        results_name = os.path.join(get_results_dir(), f"{self.model_name}_{self.hyperparameter_name}_result.json")
+        results_bestmodel_name = os.path.join(get_results_dir(), f"{self.model_name}_{self.hyperparameter_name}_result_bestmodels.pkl")
+        bestmodel_name = os.path.join(get_results_dir(), f"{self.model_name}_{self.hyperparameter_name}_bestmodel.pkl")
 
-        print(f"Saving {result_name}")
-        with open(result_name, 'w') as file:
+        print(f"Saving {results_name}")
+        with open(results_name, 'w') as file:
             json.dump(self.results(), file)
 
-        print(f"Saving {result_bestmod_name}")
-        with open(result_bestmod_name, 'wb') as fl:
+        print(f"Saving {results_bestmodel_name}")
+        with open(results_bestmodel_name, 'wb') as fl:
             pickle.dump(self.results_bestmodels(), fl)
 
-        print(f"Saving {bestmod_name}")
-        with open(bestmod_name, 'wb') as f:
+        print(f"Saving {bestmodel_name}")
+        with open(bestmodel_name, 'wb') as f:
             pickle.dump(self.best_model(), f)
 
     def results(self) -> Dict[int, Dict[float, Dict[str, float]]]:
@@ -170,114 +173,121 @@ class ClusteringModelEvaluation(ABC):
         self._is_evaluated()
         return self._best_model
 
-# todo will come to this part later about PLOT
-#     def _plot(self, title: str, res: str, y_label: str,
-#               save: bool = False, file_name: str = 'graph'):
-#         """
-#         Plot a graph foreach different kernel used:
-#             - x axes: number of component
-#             - y axes: stats (number of clusters / score / time)
-#         :param title: graph title
-#         :param res: weather score or number of cluster or time
-#         :param y_label: name for ordinates axes
-#         :param save: if to save the graph to images directory
-#         :param file_name: name of stored file
-#         """
-#
-#         # transform
-#         #   components     : hyperparameter : results
-#         #   hyperparameter : components     : results
-#         inverted_dictionary = {
-#             k: {k2: v2[k] for k2, v2 in self.results.items()}
-#             for k in self.results[list(self.results.keys())[0]]
-#         }
-#
-#         for kernel, dims in inverted_dictionary.items():
-#
-#             x = []  # number of components
-#             y = []  # result
-#
-#             for nc, out in dims.items():
-#                 x.append(nc)
-#                 y.append(out[res])
-#
-#             # Plot the points connected by a line
-#             plt.plot(x, y, '-o', label=f'{kernel}  ')
-#
-#         # Add a legend
-#         plt.legend(bbox_to_anchor=(1, 1), title=self.HYPERPARAMETER, loc='upper left', borderaxespad=0.)
-#
-#         # Set the x and y axis labels
-#         plt.title(title)
-#         plt.xlabel('Number of components')
-#         plt.ylabel(y_label)
-#
-#         # Show the plot
-#         if save:
-#             makedir(get_images_dir())
-#             file_name = path.join(get_images_dir(), f"{file_name}.{IMG_EXT}")
-#             plt.savefig(file_name)
-#
-#         # SAve the plot
-#         plt.show()
-#
-#     def plot_score(self, save=False, file_name='accuracy'):
-#         """
-#         Plot score graph
-#         :save: if to save the graph to images directory
-#         :file_name: name of stored file
-#         """
-#         self._plot(title="Random Index Score", res=self.SCORE,
-#                    y_label='Score', save=save, file_name=file_name)
-#
-#     def plot_n_clusters(self, save=False, file_name='n_clusters'):
-#         """
-#         Plot n_cluster graph
-#         :save: if to save the graph to images directory
-#         :file_name: name of stored file
-#         """
-#         self._plot(title="Varying Cluster Number", res=self.N_CLUSTERS,
-#                    y_label='NClusters', save=save, file_name=file_name)
-#
-#     def plot_time(self, save=False, file_name='time'):
-#         """
-#         Plot time execution graph
-#         :save: if to save the graph to images directory
-#         :file_name: name of stored file
-#         """
-#         self._plot(title="Elapsed Execution Time", res=self.TIME,
-#                    y_label='Time', save=save, file_name=file_name)
+
+# todo fixed but not tried
 
 
-def load_results(model_name: str, hyperparam_name: str) -> (
-        Dict[int, Dict[float, Dict[str, float]]],
-        Dict[int, Dict[str, ModelType | float]],
-        Dict[str, ModelType | float]):
-    """
+    def load_results(self):
+        """
+        """
+        # Specify the path to your JSON file
+        results_name = os.path.join(get_results_dir(), f"{self.model_name}_{self.hyperparameter_name}_result.json")
+        results_bestmodel_name = os.path.join(get_results_dir(), f"{self.model_name}_{self.hyperparameter_name}_result_bestmodels.pkl")
+        bestmodel_name = os.path.join(get_results_dir(), f"{self.model_name}_{self.hyperparameter_name}_bestmodel.pkl")
 
-    :param model_name:
-    :param hyperparam_name:
-    :return:
-    """
-    # Specify the path to your JSON file
-    result_name = os.path.join(get_results_dir(), f"{model_name}_{hyperparam_name}_result.json")
-    result_bestmod_name = os.path.join(get_results_dir(), f"{model_name}_{hyperparam_name}_result_bestmodels.pkl")
-    bestmod_name = os.path.join(get_results_dir(), f"{model_name}_{hyperparam_name}_bestmodel.pkl")
+        # Open the JSON file in read mode
+        print(f"Loading {results_name}")
+        with open(results_name, 'r') as file:
+            results = json.load(file)
 
-    # Open the JSON file in read mode
-    print(f"Loading {result_name}")
-    with open(result_name, 'r') as file:
-        result = json.load(file)
+        print(f"Loading {results_bestmodel_name}")
+        with open(results_bestmodel_name, 'rb') as fl:
+            result_bestmodels = pickle.load(fl)
 
-    print(f"Loading {result_bestmod_name}")
-    with open(result_bestmod_name, 'rb') as fl:
-        result_bestmodel = pickle.load(fl)
+        print(f"Loading {bestmodel_name}")
+        with open(bestmodel_name, 'rb') as f:
+            best_model = pickle.load(f)
 
-    print(f"Loading {bestmod_name}")
-    with open(bestmod_name, 'rb') as f:
-        best_model = pickle.load(f)
+        self._results = results
+        self._results_bestmodels = result_bestmodels
+        self._best_model = best_model
+        self._evaluated = True
 
-    return result, result_bestmodel, best_model
+
+
+
+
+# todo plot haven't started seeing this
+
+
+
+    def _plot(self, title: str, res: str, y_label: str,
+              save: bool = False, file_name: str = 'graph'):
+        """
+        Plot a graph foreach different kernel used:
+            - x axes: number of component
+            - y axes: stats (number of clusters / score / time)
+        :param title: graph title
+        :param res: weather score or number of cluster or time
+        :param y_label: name for ordinates axes
+        :param save: if to save the graph to images directory
+        :param file_name: name of stored file
+        """
+
+        # transform
+        #   components     : hyperparameter : results
+        #   hyperparameter : components     : results
+        inverted_dictionary = {
+            k: {k2: v2[k] for k2, v2 in self.results().items()}
+            for k in self.results()[list(self.results().keys())[0]]
+        }
+
+        for kernel, dims in inverted_dictionary.items():
+
+            x = []  # number of components
+            y = []  # result
+
+            for nc, out in dims.items():
+                x.append(nc)
+                y.append(out[res])
+
+            # Plot the points connected by a line
+            plt.plot(x, y, '-o', label=f'{kernel}  ')
+
+        # Add a legend
+        plt.legend(bbox_to_anchor=(1, 1), title=self.hyperparameter_name, loc='upper left', borderaxespad=0.)
+
+        # Set the x and y-axis labels
+        plt.title(title)
+        plt.xlabel('Number of components')
+        plt.ylabel(y_label)
+
+        # Show the plot
+        if save:
+            if not os.path.exists(get_images_dir()):
+                os.mkdir(get_images_dir())
+            file_name = os.path.join(get_images_dir(), f"{file_name}.{IMG_EXT}")
+            plt.savefig(file_name)
+
+        # SAve the plot
+        plt.show()
+
+    def plot_score(self, save=False, file_name='accuracy'):
+        """
+        Plot score graph
+        :save: if to save the graph to images directory
+        :file_name: name of stored file
+        """
+        self._plot(title="Random Index Score", res='score',
+                   y_label='Score', save=save, file_name=file_name)
+
+    def plot_n_clusters(self, save=False, file_name='n_clusters'):
+        """
+        Plot n_cluster graph
+        :save: if to save the graph to images directory
+        :file_name: name of stored file
+        """
+        self._plot(title="Varying Cluster Number", res='n_clusters',
+                   y_label='NClusters', save=save, file_name=file_name)
+
+    def plot_time(self, save=False, file_name='time'):
+        """
+        Plot time execution graph
+        :save: if to save the graph to images directory
+        :file_name: name of stored file
+        """
+        self._plot(title="Elapsed Execution Time", res='time',
+                   y_label='Time', save=save, file_name=file_name)
 
 
 # MEAN SHIFT
@@ -289,18 +299,14 @@ class MeanShiftEvaluation(ClusteringModelEvaluation):
         - number of components
     Provide some methods for analyzing evaluation results, such as getting the best model or plotting some trends
     """
-    model_name = "MeanShift"
+    model = "MeanShift"
     hyperparameter_name = "bandwidth"
 
-    def evaluate(self):
-        """
-        Evaluate MeanShift Clustering over all combination of
-            - number of components used
-            - kernel dimension (bandwidth)
-        """
-        self._evaluate(MeanShift(n_jobs=N_JOBS),
-                       model_name=self.model_name,
-                       hyperparam_name=self.hyperparameter_name)
+    def __init__(self, data: Dataset, n_components: List[int], hyperparam_vals: List[int | float]):
+        super().__init__(data, n_components, hyperparam_vals)
+        self.hyperparameter = self.hyperparameter_name
+        self.model_name = self.model
+        self.model_type = MeanShift(n_jobs=N_JOBS)
 
 
 # NORMALIZED CUT
@@ -311,18 +317,14 @@ class NormalizedCutEvaluation(ClusteringModelEvaluation):
         - k (number of clusters)
     Provide some methods for analyzing evaluation results, such as getting the best model or plotting some trends
     """
-    model_name = "NormalizedCut"
+    model = "NormalizedCut"
     hyperparameter_name = "n_clusters"
 
-    def evaluate(self):
-        """
-        Evaluate NormalizedCut Clustering over all combination of
-            - number of components used
-            - k (number of clusters)
-        """
-        self._evaluate(SpectralClustering(n_jobs=N_JOBS, random_state=RANDOM_SEED),
-                       model_name=self.model_name,
-                       hyperparam_name=self.hyperparameter_name)
+    def __init__(self, data: Dataset, n_components: List[int], hyperparam_vals: List[int | float]):
+        super().__init__(data, n_components, hyperparam_vals)
+        self.hyperparameter = self.hyperparameter_name
+        self.model_name = self.model
+        self.model_type = SpectralClustering(n_jobs=N_JOBS, random_state=RANDOM_SEED)
 
 
 # MIXTURE GAUSSIAN
@@ -333,15 +335,11 @@ class MixtureGaussianEvaluation(ClusteringModelEvaluation):
         - k (number of clusters)
     Provide some methods for analyzing evaluation results, such as getting the best model or plotting some trends
     """
-    model_name = "GaussianMixture"
+    model = "GaussianMixture"
     hyperparameter_name = "n_components"
 
-    def evaluate(self):
-        """
-        Evaluate MixtureGaussian Clustering over all combination of
-            - number of components used
-            - k (number of clusters)
-        """
-        self._evaluate(GaussianMixture(max_iter=200, random_state=RANDOM_SEED),
-                       model_name=self.model_name,
-                       hyperparam_name=self.hyperparameter_name)
+    def __init__(self, data: Dataset, n_components: List[int], hyperparam_vals: List[int | float]):
+        super().__init__(data, n_components, hyperparam_vals)
+        self.hyperparameter = self.hyperparameter_name
+        self.model_name = self.model
+        self.model_type = GaussianMixture(max_iter=200, random_state=RANDOM_SEED)
